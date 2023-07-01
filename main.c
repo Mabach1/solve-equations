99b18b768b86c01f83d5a3616bdbabbb718ee56f
Mabach1 <matej.baliga@gmail.com>
2023-07-01 23:34:29 +0200

    blank main

--- main.c
+++ main.c
@@ -1,19 +1,11 @@
 #include "base/base.h"
 
-i32 main(i32 argc, char **argv) {
-    if (argc < 2) {
-        ASSERT(0, "Error: Not enough arguments");
-    }
 
+i32 main(i32 argc, char **argv) {
     Arena arena;    
 
     arena_init(&arena);
 
-    const StringArr args = stringarr_from_args(&arena, argc, argv);
-    
-    string_print_format("message: {} {}\n", args.arr[1], args.arr[2]);
-
-    fprintf(stdout, "message: %s %s", string_get_lit(args.arr[1]), string_get_lit(args.arr[2]));
 
     arena_deinit(&arena);
 
