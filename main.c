f1ff8a99109b111be6813e0eff3b37444f437678
Mabach1 <matej.baliga@gmail.com>
2023-07-02 00:07:30 +0200

    improved printing function, added function for setting values in the matrix, added function for multiplying the whole matrix by single value

--- main.c
+++ main.c
@@ -7,6 +7,7 @@ typedef struct _Matrix {
 } Matrix;
 
 #define M_LEN(m) m.rows * m.cols 
+#define M_LEN_PTR(m) m->rows * m->cols 
 
 Matrix new_matrix(Arena *arena, usize rows, usize cols) {
     Matrix m = {
@@ -26,11 +27,50 @@ Matrix new_matrix(Arena *arena, usize rows, usize cols) {
 }
 
 void matrix_print(const Matrix m) {
-    for (usize i = 0; i < m.cols; ++i) {
-        for (usize j = 0; j < m.rows; ++j) {
-            fprintf(stdout,  "%.3f ", m.vals[j]);
+    u64 max_width[m.cols];
+
+    for (u64 j = 0; j < m.cols; ++j) {
+
+        max_width[j] = 0;
+
+        for (u64 i = 0; i < m.rows; ++i) {
+            u64 width = snprintf(NULL, 0, "%.2f", m.vals[i * m.cols + j]);
+            
+            if (width > max_width[j]) { max_width[j] = width; }
         }
-        fprintf(stdout, "\n");
+    }
+
+    u64 total_width = m.cols + 1; 
+    for (u64 j = 0; j < m.cols; ++j) {
+        total_width += max_width[j];
+    }
+
+
+    for (u64 i = 0; i < m.rows; ++i) {
+        fprintf(stdout, "| ");
+
+        for (u64 j = 0; j < m.cols; ++j) {
+            fprintf(stdout, "%*.*f ", max_width[j], 2, m.vals[i * m.cols + j]);
+        }
+
+        fprintf(stdout, "|\n");
+    }
+    fprintf(stdout, "\n");
+}
+
+bool matrix_set(Matrix *m, f32 *vals, usize len) {
+    if (len != M_LEN_PTR(m)) { return false; }
+
+    for (usize i = 0; i < len; ++i) {
+        m->vals[i] = vals[i];
+    }
+
+    return true;
+}
+
+void matrix_multiply_c(Matrix m, f32 c) {
+    for (usize i = 0; i < M_LEN(m); ++i) {
+        m.vals[i] *= c;
     }
 }
 
@@ -41,7 +81,11 @@ i32 main(i32 argc, char **argv) {
 
     StringArr equations = stringarr_from_file(&arena, "equations.txt");
 
-    Matrix m = new_matrix(&arena, 4, 3);
+    Matrix m = new_matrix(&arena, 3, 2);
+
+    f32 vals[] = { 0.2f, 0.45f, 0.2f, 1.f, 4.f, 69.f };
+
+    matrix_set(&m, vals, 6);
 
     matrix_print(m);
 
