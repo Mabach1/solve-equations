ceb593abbe23efd475becf6b8aecc9cc682baa10
Mabach1 <matej.baliga@gmail.com>
2024-02-04 20:00:20 +0100

    draft of solve function

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use matrix::Matrix;
 mod matrix;
 
 fn main() {
-    let m = Matrix::from_equations("1x-1y=3.5/-3x+8y=22");
-    m.print();
+    let input = r#"2x-4y=6.4/-3x+8y=22/1x+10y=20"#;
+    let m = Matrix::from_equations(input);
+    m.solve();
 }
--- src/matrix.rs
+++ src/matrix.rs
@@ -1,3 +1,4 @@
+#[derive(Clone)]
 pub struct Matrix {
     data: Vec<f32>,
     rows: usize,
@@ -71,4 +72,51 @@ impl Matrix {
         }
     }
 
+    pub fn solve(&self) -> Vec<f32> {
+        // index of values that will be 1
+        let mut idx = 0;
+
+        let mut fst_iteration_result: Vec<f32> = vec![];
+
+        for row_idx in 0..self.rows {
+            let current_row = (&self.data[row_idx * self.cols..row_idx * self.cols + self.cols]).to_vec();
+
+            // the value of element we want to be 1
+            let divisor = current_row.get(idx).unwrap().clone();
+            let divided_row = apply(&current_row, |e| e / divisor);
+
+            fst_iteration_result.append(&mut divided_row.clone());
+
+            println!("current row {:?}", divided_row);
+
+            for rest_row_idx in row_idx..self.rows - 1 {
+                let row = (&self.data[rest_row_idx * self.cols..rest_row_idx * self.cols + self.cols]).to_vec();
+
+                // modify rows                
+
+                println!("remaining row {:?}", row);
+            }
+
+            idx += 1;
+        }
+        vec![]
+    }
+}
+
+fn apply<F>(row: &Vec<f32>, func: F) -> Vec<f32>
+where
+    F: Fn(f32) -> f32,
+{
+    row.iter().map(|e| func(*e)).collect()
+}
+
+fn apply_on_rows<F>(row_a: &Vec<f32>, row_b: &Vec<f32>, func: F) -> Vec<f32>
+where
+    F: Fn(f32, f32) -> f32,
+{
+    row_a
+        .iter()
+        .zip(row_b.iter())
+        .map(|(a, b)| func(*a, *b))
+        .collect()
 }
